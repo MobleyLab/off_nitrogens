ccc399abbb6d4096ab35f6bd27287eb0ef330d66
jmaat <jmaat@uci.edu>
2019-01-31 13:40:36 -0800

    Updated datap processing code and plotting function

--- ForceBalance_12mol_plots/allData/process_QMMM.py
+++ ForceBalance_12mol_plots/allData/process_QMMM.py
@@ -1,18 +1,13 @@
-###WIP:
 ### This script will take an .xyz (scan-final.xyz) from the finished QM torsion scan and generate/process data to create a plot of QM and MM energies.
-
-
 #Imports
 from openforcefield.typing.engines.smirnoff import *
 from openforcefield.utils import get_data_filename, extractPositionsFromOEMol, generateTopologyFromOEMol
 from openeye.oechem import *
-#import oenotebook as oenb
 from openeye.oeomega import * # conformer generation
 from openeye.oequacpac import * #for partial charge assignment
-from calc_improper import *
+from calc_improper import find_improper_angles, calc_improper_angles, angle_betwen
 import numpy as np
 import matplotlib.pyplot as plt
-
 
 #Functions
 def SDF2oemol(sdffile):
@@ -34,7 +29,6 @@ def SDF2oemol(sdffile):
     molecules = list()
     oechem.OEReadMolecule(ifs, oemol)
     ifs.close()
-    print(oemol.GetCoords())
     return oemol
 
 def smiles2oemol(smiles):
@@ -99,7 +93,7 @@ def QM2Oemol(xyzfile, oemol):
     return oemolList
 
 
-def GetMM(oemol, FF, FFRmNit):
+def GetMM(oemol, FF, tag):
     """
     Description:
     Takes in an oemol and calculates the MM energies with two forcefields, one which has removed
@@ -109,10 +103,10 @@ def GetMM(oemol, FF, FFRmNit):
     Input:
     oemol: A single oemol object
     FF: .offxml file of smirnoff99Frosst.offxml
-    FFRmNit: .offxml file of smirnoff99Frosst.offxml with the removed nitrogen improper parameter
+    tag: The name to tag the energy as, ex: "MM"
 
     Return:
-    oemol: An oemol with the MM energies stored in tags MM and MMRmNit in kcal/mol
+    oemol: An oemol with the MM energies stored in tag in units kcal/mol
     """
 
     #prep both force fields, create system and topology, get MM energies and store in data tag
@@ -125,21 +119,7 @@ def GetMM(oemol, FF, FFRmNit):
     simulation.context.setPositions(positions)
     state = simulation.context.getState(getEnergy = True, getPositions=True)
     energy = state.getPotentialEnergy() / unit.kilocalories_per_mole
-    print("this is the original mm energy: " + str(energy))
-    oemol.SetData("MM", energy)
-
-    #repeat with removed nitrogen
-    ffn = ForceField(FFRmNit)
-    topology = generateTopologyFromOEMol(oemol)
-    system_n = ffn.createSystem(topology, [oemol])
-    positions = extractPositionsFromOEMol(oemol)
-    integrator = openmm.VerletIntegrator(2.0*unit.femtoseconds)
-    simulation = app.Simulation(topology, system_n, integrator)
-    simulation.context.setPositions(positions)
-    state = simulation.context.getState(getEnergy = True, getPositions=True)
-    energy_rm = state.getPotentialEnergy() / unit.kilocalories_per_mole
-    print("this is the original mm energy: " + str(energy))
-    oemol.SetData("MMRmNit", energy_rm)
+    oemol.SetData(tag, energy)
 
     return oemol
 
@@ -170,13 +150,11 @@ def findAngles(oemol, constraint):
         if "dihedral" in l:
             split = l.split()
             atoms_imp = (int(split[1])-1, int(split[2])-1, int(split[3])-1, int(split[4])-1)
-            print(atoms_imp)
             crd1 = np.asarray(coords[atoms_imp[0]])
             crd2 = np.asarray(coords[atoms_imp[1]])
             crd3 = np.asarray(coords[atoms_imp[2]])
             crd4 = np.asarray(coords[atoms_imp[3]])
             angle_imp = calc_improper_angle(crd1, crd2, crd3, crd4, True)
-            print(angle_imp)
             if angle_imp < 90 and angle_imp > 0:
                 oemol.SetData("improper", angle_imp)
             if angle_imp < 0:
@@ -186,7 +164,6 @@ def findAngles(oemol, constraint):
             if angle_imp > 90:
                 angle_imp = angle_imp - 180
                 oemol.SetData("improper", angle_imp)
-            print(angle_imp)
 
         #calculate the valence angle and store in tag (2-d scan)
         if "angle" in l:
@@ -224,7 +201,6 @@ def makeOEB(oemolList, tag):
 
 
 
-
 def adjust_energy(oemolList, qm_tag, mm_tag):
     """
     Description:
@@ -249,7 +225,6 @@ def adjust_energy(oemolList, qm_tag, mm_tag):
     #get correspoding lowest mm energy
     #low_mol_mm = sorted(oemolList, key=lambda x: x.GetData(mm_tag))[0]
     low_mm = low_mol.GetData(mm_tag)
-    print("this is the low mm value:" + str(low_mm))
 
     #iterate through the list and subtract lowest mm and qm energies
     for m in oemolList:
@@ -261,27 +236,32 @@ def adjust_energy(oemolList, qm_tag, mm_tag):
     return oemolList
 
 
-def plotResults(oemolList):
+
+
+def plotResultsMulti(oemolList, names, tagList, colorList, markers):
     """
-    Description: This function plots the QM and MM data for 1d torsion scans
+    Description: This function plots data for multiple molecules in a tag list.
 
     Input:
-    oemolList: List of eomols with stored angles, MM, and QM energies
+    oemolList: List of oemols with tags of energy data
+    names: List of molecule names to title plot
+    tagList: List of tagged data of interest
+    colorList: List of colors for plot corresponding with the tags
+    markers: Specified marker types for the plot
     """
-    #sort oemolList based on the improper angles
-    sort_oemol = sorted(oemolList, key=lambda x: x.GetData('improper'))
+    for mols, title in zip(oemolList, names):
+        sort_oemol = sorted(mols, key=lambda x: x.GetData('improper'))
+        xs = [m.GetData('improper') for m in sort_oemol]
+        qm = [m.GetData('QM') for m in sort_oemol]
+        plt.plot(xs, qm, color="royalblue", label='QM', marker= "^", markersize="8", linewidth="3")
+        for tag, col, mar in zip(tagList, colorList, markers):
+            trend = str(tag)
+            name = [m.GetData(tag) for m in sort_oemol]
+            plt.plot(xs, name, color=col, label=trend, marker=mar,markersize="8", linewidth="3")
+        plt.title(title)
+        plt.legend()
+        plt.show()
 
-    xs = [m.GetData('improper') for m in sort_oemol]
-    qm = [m.GetData('QM') for m in sort_oemol]
-    mm = [m.GetData('MM') for m in sort_oemol]
-    mm_nit = [m.GetData('MMRmNit') for m in sort_oemol]
-
-    plt.plot(xs, qm, color="royalblue", label='QM', marker='^',markersize="8", linewidth="3")
-    plt.plot(xs, mm, color="red", label='MM', marker='^',markersize="8", linewidth="3")
-    plt.plot(xs, mm_nit, color="orange", label='MM removed Imp.', marker='^',markersize="8", linewidth="3")
-    #plt.plot(xs, qm, color="royalblue", label='Carbon 1', marker='^',markersize="12", linewidth="5")
-    plt.legend()
-    plt.show()
 
 
 def oeb2mollist(oeb):
@@ -311,7 +291,7 @@ def oeb2mollist(oeb):
 
 
 
-def compareGromacs(oemolList, FF, top, gro):
+def compareGromacs(oemolList, FF, topfile, grofile):
     """
     Description:
     Compares energies of openMM to gromacs
@@ -319,28 +299,86 @@ def compareGromacs(oemolList, FF, top, gro):
     input:
     oemolList: List of oemol objects to compare
     FF: openmm force field, .offxml file
-    top: GROMACS topology file name to write
-    gro: GROMACS coordinate file name (.gro format) to write
+    topfile: GROMACS topology file name to write
+    grofile: GROMACS coordinate file name (.gro format) to write
 
 
     """
-    for mol in oemolList:
+
+    for idx, mol in enumerate(oemolList):
+        name = str(idx) + "_" + topfile
+        name_gro = str(idx) + "_" + grofile
         ff = ForceField(FF)
         topology = generateTopologyFromOEMol(mol)
         system = ff.createSystem(topology, [mol])
         positions = extractPositionsFromOEMol(mol)
-        save_system_to_gromacs( topology, system, positions, top, gro )
+        save_system_to_gromacs(topology, system, positions, name, name_gro)
+
+
+        top = parmed.load_file(name)
+        gromacssys = top.createSystem(nonbondedMethod= app.NoCutoff, constraints = None, implicitSolvent = None)
+        gro = parmed.load_file(name_gro)
+
+        #smirnoff
+        smirfftop, smirffsys, smirffpos = create_system_from_molecule(ff, mol, verbose = False)
+
+        print(oechem.OEMolToSmiles(mol))
+        #compare
+        try:
+            groups0, groups1, energy0, energy1 = compare_system_energies( smirfftop, top.topology, smirffsys, gromacssys, positions, verbose = False)
+            print(groups0, groups1, energy0, energy1)
+        except:
+            print(oechem.OEMolToSmiles(mol))
+            print("Failed")
 
 
 
 
-#Main
-def processData(smiles, xyzfile, constraintFile, moltitle, FF, FFRmNit):
+
+def sdf2mol2(sdf, output):
+    """
+    Takes in .sdf file and writes to a .mol2 file
+    input:
+    sdf: .sdf file
+    output: name of output .mol2 file
+    """
+
+    ifs = oechem.oemolistream(sdf)
+    ofs = oechem.oemolostream(output)
+
+    ifs.SetFormat(oechem.OEFormat_SDF)
+    ofs.SetFormat(oechem.OEFormat_MOL2)
+
+    for mol in ifs.GetOEGraphMols():
+            oechem.OEWriteMolecule(ofs, mol)
+
+
+
+def oeb2oemol(oebfile):
+    """
+    Takes in oebfile and generates oemolList
+    input:
+    oebfile: Oebfile (.oeb)
+
+    return:
+    mollist: List of oemols
+
+    """
+
+    ifs = oechem.oemolistream(oebfile)
+    mollist = []
+
+    for mol in ifs.GetOEGraphMols():
+            mollist.append(oechem.OEGraphMol(mol))
+
+    return mollist
+
+
+def processData(smiles, xyzfile, constraintFile, moltitle, FF, tags, color):
     """
     Description:
     Takes in innitial .sdf file, xyzfile from geomeTRIC output, constraint file and a title to
     create an .oeb file with oemols with QM, and MM data.
-    Also generates plot comparing QM and MM energies.
 
 
     input:
@@ -349,8 +387,9 @@ def processData(smiles, xyzfile, constraintFile, moltitle, FF, FFRmNit):
     xyzfile: output .xyz file from geomeTRIC scan, scan-final.xyz that contains QM energies in title
     constraintFile: constraint.txt for geomeTRIC input that contains the indicies constrained in scan
     molTitle: title for the output .oeb file
-    FF: Forcefield
-    FFRmNit: Force field with removed nitrogens
+    FF: Forcefield list
+    tags: List of tags corresponding to force field
+    color: List of colors used for plot
 
     Return:
     none, generates .oeb file and plot of data
@@ -359,34 +398,83 @@ def processData(smiles, xyzfile, constraintFile, moltitle, FF, FFRmNit):
     #generate lsit of oemols with stored QM energies
     oemolList = QM2Oemol(xyzfile, SDF2oemol(smiles))
 
-    #get MM energies and improper angles from geometries (and potentially valence depending no the scan)
-    for mol in oemolList:
-        GetMM(mol, FF, FFRmNit)
-        findAngles(mol, constraintFile)
+    #get MM energies from force fields
+    for f, tag in zip(FF,tags):
+        for mol in oemolList:
+            print("THIS IS THE FORCE FIELD" + str(f))
+            GetMM(mol, f, tag)
+            findAngles(mol, constraintFile)
 
     #normalize eneriges:
-    adjust_energy(oemolList, 'QM', 'MM')
-    adjust_energy(oemolList, 'QM', 'MMRmNit')
+    for tag in tags:
+        adjust_energy(oemolList, 'QM', tag)
 
     #write out oeb file
     makeOEB(oemolList, moltitle)
-    #plot results
-    plotResults(oemolList)
 
-    return
+    return oemolList
 
 
+def makeInputs(molName):
+    '''
+    This function makes the input files in a format to process MM and QM energies of groups of molecules
+
+    Input:
+    molName: List of molecules name in a set of molecules
+
+    Return
+    molName: Original molName list
+    SDFList: List of sdf files for molecule in list
+    contList: List of constraint files for molecule
+    XYZList: List of .xyz scan files
+    '''
+    SDFList = []
+    contList = []
+    XYZList = []
+
+    for name in molName:
+        sdfName = str(name) + '.sdf'
+        SDFList.append(sdfName)
+
+        contName = 'c-' + str(name) + '.txt'
+        contList.append(contName)
+
+        XYZName = 'scan-final-' + str(name) + '.xyz'
+        XYZList.append(XYZName)
+
+    return molName, SDFList, contList, XYZList
 
 
+def processSet(filesList, molNames, FFList, tagNames, colorList, markerList):
+    '''
+    This function takes in information for a set of molecules and generates
+    subplots for all of the molecules from MM energies from the specified FFs.
 
+    input:
+    filesList: A list of molNames, SDF file of starting molecule, constraints file list, xyz files with QM scans
+    molNames: List of oemol names
+    FFList : List of FFs of interest
+    tagNames: name of tags associated with FFs
+    colorList: List of colors for the plot
+    markerList: Type of markers for pots
+    '''
 
+    names = filesList[0]
+    sdf = filesList[1]
+    cont = filesList[2]
+    xyz = filesList[3]
 
-#plotResults(oeb2mollist('results.oeb'))
+    mols = []
 
-compareGromacs(QM2Oemol('scan-final.xyz', SDF2oemol('mol6.sdf')), 'smirnoff99Frosst.offxml', 'topology', 'coordfile')
+    i = 0
+    while i < len(filesList[0]):
+        doneList = processData(sdf[i], xyz[i], cont[i], names[i], FFList, tagNames, colorList)
+        mols.append(doneList)
+        i += 1
 
-#test
-#processData('mol6.sdf', 'scan-final.xyz', 'constraints.txt', 'fixresults', 'smirnoff99Frosst.offxml', 'nitRem.offxml')
+    #plot the list of list of oemols
+    plotResultsMulti(mols, molNames, tagNames, colorList, markerList)
+
 
 
 
